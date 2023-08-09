b39020dcfbff47749ad806d709733b10b99a7f05
haydentinker <haydentinker613@gmail.com>
2023-08-09 11:38:19 -0700

    Gltf loader

--- portfolio_website/src/App.js
+++ portfolio_website/src/App.js
@@ -1,29 +1,52 @@
-import React from 'react';
-import { Canvas, useThree } from 'react-three-fiber';
-import { OrbitControls, useGLTF } from '@react-three/drei';
+import { useEffect } from 'react';
 
-import { GLTFLoader } from 'three/addons/loaders/GLTFLoader';
+import * as THREE from 'three';
+// import { FBXLoader } from 'three/examples/jsm/loaders/FBXLoader';
+// import { OBJLoader } from 'three/examples/jsm/loaders/OBJLoader';
+// import { VOXLoader } from 'three/examples/jsm/loaders/VOXLoader';
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
 
-// Custom component to display the GLTF model
-const GLTFModel = () => {
-  const { scene } = useGLTF('./objects/scene.gltf'); // Load the entire scene
+import SceneInit from './SceneInit';
+
+function App() {
+  useEffect(() => {
+    const test = new SceneInit('myThreeJsCanvas');
+    test.initialize();
+    test.animate();
+
+    // const boxGeometry = new THREE.BoxGeometry(8, 8, 8);
+    // const boxMaterial = new THREE.MeshNormalMaterial();
+    // const boxMesh = new THREE.Mesh(boxGeometry, boxMaterial);
+    // test.scene.add(boxMesh);
+
+    let loadedModel;
+    const glftLoader = new GLTFLoader();
+    glftLoader.load('./assets/david/scene.gltf', (gltfScene) => {
+      loadedModel = gltfScene;
+      console.log(loadedModel);
+
+      gltfScene.scene.rotation.y = Math.PI / 8;
+      gltfScene.scene.position.y = 3;
+      gltfScene.scene.scale.set(10, 10, 10);
+      test.scene.add(gltfScene.scene);
+    });
+
+    const animate = () => {
+      if (loadedModel) {
+        loadedModel.scene.rotation.x += 0.01;
+        loadedModel.scene.rotation.y += 0.01;
+        loadedModel.scene.rotation.z += 0.01;
+      }
+      requestAnimationFrame(animate);
+    };
+    animate();
+  }, []);
 
   return (
-    <primitive object={scene} />
+    <div>
+      <canvas id="myThreeJsCanvas" />
+    </div>
   );
-};
-
-
-// Main component
-const App = () => {
-  return (
-    <Canvas>
-      <ambientLight intensity={0.5} />
-      <pointLight position={[10, 10, 10]} />
-      <GLTFModel />
-      <OrbitControls />
-    </Canvas>
-  );
-};
+}
 
 export default App;
