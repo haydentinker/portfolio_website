90b6d9b4889c8f5a3092fa8da798366f6d0ef8fc
haydentinker <haydentinker613@gmail.com>
2023-08-14 11:44:21 -0700

    Home Component responsive and functional

--- portfolio_website/src/components/Home.jsx
+++ portfolio_website/src/components/Home.jsx
@@ -20,17 +20,39 @@ const ResponsiveText = ({ position, scale, children }) => {
 
 export const Home = () => {
   const { viewport } = useThree();
-  const isMobile = window.innerWidth < 768;
+  const isMobile = window.innerWidth < 769;
+  const isDesktop= !isMobile && window.innerWidth<2560;
   const responsiveRatio = viewport.width
-  console.log(viewport.height);
   return (
     <>
 
-      <ambientLight intensity={1} />
-      {!isMobile ?( <> <DeskSetupModel position={[3, 0.5, -2]} scale={[0.15, 0.15, 0.15]} />
-        <ResponsiveText position={[-7, 1, -3]} scale={[0.07 * responsiveRatio, 0.07 * responsiveRatio, 0.07 * responsiveRatio]}>Hello! I'm</ResponsiveText>
-        <ResponsiveText position={[-viewport.width/3, -viewport.height / 2.5 , 0]} scale={[0.08 * responsiveRatio, 0.08 * responsiveRatio, 0.08 * responsiveRatio]}>Hayden Tinker</ResponsiveText></>):
-          <ResponsiveText position={[-0.75, isMobile ? -viewport.height / 6 : 0, 0]} scale={[0.09 * responsiveRatio, 0.09 * responsiveRatio, 0.09 * responsiveRatio]}>Hayden Tinker</ResponsiveText>}
+      <ambientLight intensity={.7} />
+      <directionalLight intensity={2} position={[0,viewport.height,1]}/>
+      {!isMobile ?( 
+        isDesktop?(
+
+      <> 
+      
+        <DeskSetupModel position={[viewport.width/2, viewport.height/6, -4]} scale={[0.02 * responsiveRatio, 0.02 * responsiveRatio, 0.02 * responsiveRatio]} />
+        <ResponsiveText position={[-viewport.width/3, .5, 0]} scale={[0.07 * responsiveRatio, 0.07 * responsiveRatio, 0.07 * responsiveRatio]}>Hello! I'm</ResponsiveText>
+        <ResponsiveText position={[-viewport.width/3, -viewport.height / 5 , 0]} scale={[0.08 * responsiveRatio, 0.08 * responsiveRatio, 0.08 * responsiveRatio]}>Hayden Tinker</ResponsiveText>
+        </>):
+        <> 
+        <DeskSetupModel position={[viewport.width/3, viewport.height/5, -4]} scale={[0.01 * responsiveRatio, 0.01 * responsiveRatio, 0.01* responsiveRatio]} />
+        <ResponsiveText position={[-viewport.width/3, viewport.height/6, 0]} scale={[0.07 * responsiveRatio, 0.07 * responsiveRatio, 0.07 * responsiveRatio]}>Hello! I'm</ResponsiveText>
+        <ResponsiveText position={[-viewport.width/3, -viewport.height / 5 , 0]} scale={[0.08 * responsiveRatio, 0.08 * responsiveRatio, 0.08 * responsiveRatio]}>Hayden Tinker</ResponsiveText>
+        </>
+        ):
+
+          //Mobile design
+          <>
+          <ResponsiveText position={[-viewport.width/7, viewport.height/4.5, 1]} scale={[0.09 * responsiveRatio, 0.09 * responsiveRatio, 0.09 * responsiveRatio]}>Hello,</ResponsiveText>
+          <ResponsiveText position={[-viewport.width/12, viewport.height /7, 1]} scale={[0.09 * responsiveRatio, 0.09 * responsiveRatio, 0.09 * responsiveRatio]}>I'm</ResponsiveText>
+          <DeskSetupModel position={[-viewport.width/12, 0, -7]}scale={[0.15, 0.15, 0.15]} />
+          <ResponsiveText position={[-viewport.width/5, -viewport.height/7.5, 2]} scale={[0.09 * responsiveRatio, 0.09 * responsiveRatio, 0.09 * responsiveRatio]}>Hayden</ResponsiveText>
+          <ResponsiveText position={[-viewport.width/5, -viewport.height/4.5, 2]} scale={[0.09 * responsiveRatio, 0.09 * responsiveRatio, 0.09 * responsiveRatio]}>Tinker</ResponsiveText>
+          </>
+          }
     </>
       
   );
