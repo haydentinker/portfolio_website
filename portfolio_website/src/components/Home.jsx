ffb401ad076977b9bd108aae178f189c059e61f2
haydentinker <haydentinker613@gmail.com>
2023-08-13 21:31:53 -0700

    Commit

--- portfolio_website/src/components/Home.jsx
+++ portfolio_website/src/components/Home.jsx
@@ -1,40 +1,48 @@
 import React from 'react';
-import { Canvas } from '@react-three/fiber';
-import { Typography, Container, Box } from "@mui/material";
-import { Volleyball } from '../models/Volleyball';
-import { amber } from '@mui/material/colors';
-import { AmbientLight } from 'three';
+import { Canvas} from '@react-three/fiber';
 import { DeskSetupModel } from '../models/DeskSetupModel';
+import { useTheme } from '@emotion/react';
+import { Text3D } from '@react-three/drei';
+
 
 export const Home = () => {
+  const materialUiTheme=useTheme();
+  const font = materialUiTheme.typography.fontFamily;
   return (
-    <Container
-      sx={{
-        marginTop: '10%',
-      }}
-    >
-      <Typography
-        variant='h1'
-        gutterBottom
-        noWrap
-        style={{ position: 'absolute', zIndex: '1' }}
-      >
-        Hi,
-        <br />
-        I'm Hayden,
-        <br />
-        Software Engineer
-      </Typography>
-      <Canvas
-        style={{
-  
-          backgroundColor: 'transparent',
-          zIndex: '-5',
-        }}
-      >
-        <ambientLight intensity={1} />
-        <DeskSetupModel scale={[0.15, 0.15, 0.15]} />
-      </Canvas>
-    </Container>
+    <Canvas
+    style={{
+      height:'100vh',
+      width:'100%',
+      backgroundColor: 'transparent',
+    }}
+  >
+    <ambientLight intensity={1} />
+    <DeskSetupModel  position={[3,.5,-2]}scale={[0.15, 0.15, 0.15]}  />
+    <Text3D  position={[-7,1,-3]} font={
+      'https://cdn.rawgit.com/mrdoob/three.js/r129/examples/fonts/helvetiker_regular.typeface.json'
+    }>
+      Hello! I'm 
+      <meshStandardMaterial attach='material' color="pink" />
+    </Text3D>
+    <Text3D  position={[-4.5,-2,1]}  font={
+      'https://cdn.rawgit.com/mrdoob/three.js/r129/examples/fonts/helvetiker_regular.typeface.json'
+    }>
+      Hayden Tinker
+      <meshStandardMaterial attach='material' color="pink" />
+    </Text3D>
+  </Canvas>
+      // {/* <Typography
+      //   variant='h1'
+      //   gutterBottom
+      //   noWrap
+      //   style={{ position: 'relative', zIndex: '1' }}
+      // >
+      //   Hi,
+      //   <br />
+      //   I'm Hayden,
+      //   <br />
+      //   Software Engineer
+      // </Typography> */}
+
   );
 };
