9a4ab45a7397f7da7323d44a2c5ca46af15ee904
haydentinker <haydentinker613@gmail.com>
2023-08-28 12:06:20 -0700

    Tests for material ui components

---
+++ portfolio_website/src/tests/experience.test.jsx
@@ -0,0 +1,22 @@
+import {render,screen} from '@testing-library/react';
+import { describe,test} from 'vitest';
+import { Experience } from '../components/Experience';
+import '@testing-library/jest-dom';
+import 'intersection-observer';
+describe('Experience Component', () => {
+ 
+
+  test('renders Experience Component and checks to make sure timeline element is in document', async() => {
+    const { container,getByText}  = render(<Experience/>);
+    
+    const textContent = getByText('Experience');
+    await expect(textContent).toBeInTheDocument();
+    const timeline=container.querySelectorAll('.MuiTimeline-root');
+    await expect(timeline[0]).toBeInTheDocument();
+
+    const experienceEvents=container.querySelectorAll('.MuiTimelineContent-root');
+    experienceEvents.forEach( async(event) => {
+        await expect(event).toBeInTheDocument();
+      });
+  });
+});
